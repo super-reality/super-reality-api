d47d9fcac92de61efea4d0bd7b6eb1fe7e9c59b2
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-09-29 20:59:46 +0600

    added logger

--- migrations/20200919161452-stepCollection.js
+++ migrations/20200919161452-stepCollection.js
@@ -1,49 +1,13 @@
 module.exports = {
     async up(db, client) {
-        await db.createCollection("steps", {
-            validator: {
-                $jsonSchema: {
-                    bsonType: "object",
-                    required: [
-                        "name",
-                        "description"
-                    ],
-
-                    properties: {
-                        // step Image
-                        image: {bsonType: "string"},
-                        // image function
-                        imageFunction: {bsonType: "int"},
-                        // additional image function
-                        additionalFunctions: {bsonType: "array"},
-                        // step name
-                        name: { bsonType: "string" },
-                        // step long description
-                        description: { bsonType: "string"},
-                        // trigger
-                        trigger: {bsonType: "int"},
-                        // next Step action
-                        next: {bsonType: "int"},
-                        // user id that created this step
-                        createdBy: {bsonType: "objectId"},
-                        // created date
-                        createdAt: {bsonType: "date"},
-                        updatedAt: {bsonType: "date"},
-
-                    }
-                }
-            }
-
-        }).then(function (steps) {
-            steps.createIndex({
-                name: 1,
-            })
-        }).catch(function (err) {
-            console.error(err)
+        await db.steps.createIndex({
+            name: 1,
         })
     },
 
     async down(db, client) {
-        await db.dropCollection("steps")
+        await db.steps.createIndex({
+            name: 1,
+        })
     },
 };
--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const auth = require("./middleware/auth");
+const morgan = require("morgan")
+const fs = require("fs")
+const path = require("path");
+
+
 
 const mongoose = require("mongoose");
 mongoose.Promise = Promise;
 const database = mongoose.connection;
 let apiRoutes = require("./routes/api/v1/");
 
-const {hashDigest, hashSaltDigest} = require("./utilities/hashing");
 
 app.use(express.json());
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(morgan('combined', { stream: accessLogStream }))
+
 
 // setting for local server connection from other port
 app.use(function (req, res, next) {
@@ -33,5 +40,4 @@ server.listen(port, () => console.log(`listening on port ${port}`));
 database.on("error", () => console.error("database connection error"));
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
-
 module.exports = {server}
