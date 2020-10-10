646203e6475214ee8daba5bdc2f17792b30fb2dc
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-10-10 18:19:25 +0600

    package updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,7 @@ module.exports = {
     apps: [{
         script: 'server.js',
         watch: '.',
-        restart_delay: 10000
-
+        restart_delay: 10000,
     },],
     env_production: {
          PORT:"3000",
