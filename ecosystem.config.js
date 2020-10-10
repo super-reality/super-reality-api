161212876149450be2aee3e5113fc0de876192cb
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-10-10 18:29:13 +0600

    server config updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,15 @@ module.exports = {
         script: 'server.js',
         watch: '.',
         restart_delay: 10000,
+        exp_backoff_restart_delay: 100,
+        error_file: 'err.log',
+        out_file: 'out.log',
+        log_file: 'combined.log',
+        time: true,
+        kill_timeout: 3000
     },],
     env_production: {
-         PORT:"3000",
+        PORT: "3000",
         NODE_ENV: "development",
     }
 };
