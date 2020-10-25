4694404242b6dcabdfa25f3788c7bf6ab088a7f3
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-10-25 09:06:36 +0600

    api documentation added for sign up and sign in

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ database.on("error", () => console.error("database connection error"));
 
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 const app= require("./app")
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./configuration/swagger')
+
+// use swagger-Ui-express for your app documentation endpoint
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 const server = require("http").createServer(app);
 
 const port = process.env.PORT;
