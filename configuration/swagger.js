f14d61a4cab89f0aceafae288c60cdc551308b6c
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-10-25 09:10:07 +0600

    update server address

--- configuration/swagger.js
+++ configuration/swagger.js
@@ -6,7 +6,7 @@ const swaggerDefinition = {
     version: '1.0.0', // Version of the app
     description: 'This is the REST API for my product', // short description of the app
   },
-  host: 'localhost:3000', // the host or url of the app
+  host: 'http://54.183.151.100:3000', // the host or url of the app
   basePath: '/api/v1', // the basepath of your endpoint
 };
 
