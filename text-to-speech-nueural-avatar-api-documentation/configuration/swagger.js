61cc0b2893a384caf9bf4a830d4947c2edef0ace
sifanduronto <sifan@sifans-MacBook-Air.local>
2020-10-27 04:24:33 +0600

    api-documentation added for text to speech

---
+++ text-to-speech-nueural-avatar-api-documentation/configuration/swagger.js
@@ -0,0 +1,21 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const swaggerDefinition = {
+  info: {
+    title: 'Super Reality Api', // Title of the documentation
+    version: '1.0.0', // Version of the app
+    description: 'Convert your reality to super reality', // short description of the app
+  },
+  host: ': http://54.153.19.166:5000', // the host or url of the app
+  basePath: '/api/v1', // the basepath of your endpoint
+};
+
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./docs/**/*.yaml'],
+};
+// initialize swagger-jsdoc
+module.exports = swaggerJSDoc(options);
