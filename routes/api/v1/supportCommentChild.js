0c66b835b3d3ec4e4b90f6e3d5126b6bec532ae8
sifanduronto <sifan@sifans-MacBook-Air.local>
2021-06-02 21:14:36 +0600

    support comment url added

--- routes/api/v1/supportCommentChild.js
+++ routes/api/v1/supportCommentChild.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const router = express.Router()
 const auth = require("../../../middleware/auth")
-const {createComment, getCommentsByTicket,deleteCommentsByTicket} = require('../../../controllers/supportComment')
+const {createComment, getCommentsByTicket, deleteCommentsByTicket} = require('../../../controllers/supportComment')
 
 router.post("/create", auth(), function (req, res) {
 
@@ -15,6 +15,12 @@ router.get("/:id", auth(), function (req, res) {
 
 })
 router.delete("/:id", auth(), function (req, res) {
+
+    deleteCommentsByTicket(req, res)
+
+})
+
+router.put("/:id", auth(), function (req, res) {
 
     deleteCommentsByTicket(req, res)
 
